23bf21c5d8c39b95759dd1b8cb0e139d919f7a86
Bruno Hebert <brunovesp30@gmail.com>
2025-12-02 17:29:07 -0300

    adapter

--- back-end/prisma.config.ts
+++ back-end/prisma.config.ts
@@ -2,8 +2,4 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
     schema: './prisma/schema.prisma',
-    adapter: {
-        provider: 'postgresql',
-        url: process.env.DATABASE_URL!,
-    },
 });
